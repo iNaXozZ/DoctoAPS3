744c30ce3ee7d68d9f0a1af9dd699b11c284677c
iNaXozZ <79304081+iNaXozZ@users.noreply.github.com>
2021-12-11 17:41:40 +0100

    Méthode GetSesConsultations + twig

--- src/Controller/ProfessionnelDeSanteController.php
+++ src/Controller/ProfessionnelDeSanteController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProfessionnelDeSanteRepository;
+use App\Entity\ProfessionnelDeSante;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +15,27 @@ class ProfessionnelDeSanteController extends AbstractController
      */
     public function GetLesProfessionnels(ProfessionnelDeSanteRepository $ProfessionnelDeSanteRepository): Response
     {
+        // Récupération de toutes les instances de ProfessionnelDeSante
         $lesprofessionnels = $ProfessionnelDeSanteRepository->findAll();
         return $this->render('professionnelDeSante/afficherlespros.html.twig', [
             'lesprofessionnels'=> $lesprofessionnels,
+        ]);
+    }
+
+    /**
+     * @Route("/connect/affichersesconsulations/{id}", name="afficherconsultations")
+     */
+    public function GetSesConsultations($id): Response
+    {
+        // Récupération de l'id courant de l'utilisateur ProfessionnalDeSante
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $entityManager->getRepository(ProfessionnelDeSante::class)->find($id);
+        // Récupération des consultations de l'utilisateur courant
+        $user->getLesConsultations();
+        
+        return $this->render('professionnelDeSante/afficherconsultation.html.twig', [
+            'lesconsultations' => $user->getLesConsultations(),
+            'controller_name' => 'AfficherConsultation',
         ]);
     }
 }
